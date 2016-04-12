61cc236ac85ab781e3a9e58d47ca5c457db9eff6
Uri Shaked <uri@salsa4fun.co.il>
2016-04-12 23:10:55 +0300

    feat(example): add Blink example
    also prepare for publishing on npm

--- README.md
+++ README.md
@@ -3,5 +3,21 @@
 # Angular 2 IoT
 > IoT support for Angular 2
 
+# Building & Running the Blink example
+
+The [Blink Example](https://github.com/urish/angular2-iot/blob/master/examples/blink.ts) will blink the built-in 
+LED on an Adruino board that is connected to your PC. You will need to upload the 
+[StandardFirmata firmware](https://github.com/firmata/arduino) to your Arduino board first. 
+
+    git clone https://github.com/urish/angular2-iot
+    npm install
+    npm run build
+    npm run example:build
+    npm run example:run
+    
+Note: The example program will try to detect the serial port that the Arduino 
+is connected to automatically. You can manually specify the port name by 
+setting the `SERIAL_PORT` environment variable prior to running the example. 
+
 # License
 [![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](/LICENSE)
---
+++ examples/blink.ts
@@ -0,0 +1,39 @@
+/**
+ * A Blink example for Angular IoT. Connect it to an Arduino board with
+ * a Firmata firmware to start linking the built-in LED (connected to pin
+ * number 13).
+ * 
+ * You may need to specify your serial port name by settings the environment
+ * variable SERIAL_PORT.
+ * 
+ * Copyright (C) 2016, Uri Shaked. License: MIT.
+ */
+
+import 'angular2-universal-preview/polyfills';
+
+import {Component} from 'angular2/core';
+import {bootstrap} from '../src/index';
+import {Board} from 'johnny-five';
+
+@Component({
+  template: `
+    <iot-led pin="13" [state]="ledState"></iot-led>
+  `
+})
+class IotBlinkExample {
+  private ledState: boolean = false;
+
+  constructor() {
+    setInterval(() => {
+      this.ledState = !this.ledState;
+    }, 500);
+  }
+}
+
+const board = new Board({
+  port: process.env.SERIAL_PORT
+});
+
+board.on('ready', () => {
+  bootstrap(IotBlinkExample);
+});
